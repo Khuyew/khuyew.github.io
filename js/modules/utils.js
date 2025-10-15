47a437036710c2ac0f29be19bd3e3e104b00a3f5
Хуйевъ <khuyew@proton.me>
2025-10-15 11:41:30 +0300

    Update and rename js/utils.js to js/modules/utils.js

---
+++ js/modules/utils.js
@@ -0,0 +1,27 @@
+class Utils {
+    static debounce(func, wait, immediate = false) {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                timeout = null;
+                if (!immediate) func(...args);
+            };
+            const callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func(...args);
+        };
+    }
+
+    static throttle(func, limit) {
+        let inThrottle;
+        return function(...args) {
+            if (!inThrottle) {
+                func.apply(this, args);
+                inThrottle = true;
+                setTimeout(() => inThrottle = false, limit);
+            }
+        };
+    }
+
+    static escapeHtml(unsafe
