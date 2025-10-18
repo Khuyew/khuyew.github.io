f21eabbfb972f55c5ac097f0ad4c6fa119174d17
Кхуйев <khuyew@proton.me>
2025-10-18 19:43:09 +0300

    Update sw.js
    Signed-off-by: Кхуйев <khuyew@proton.me>

--- new/sw.js
+++ new/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'khai-assistant-v2.0.0';
+const CACHE_NAME = 'khai-assistant-v1.0.0';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -12,67 +12,36 @@ const urlsToCache = [
   'https://js.puter.com/v2/'
 ];
 
-// Install event
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Service Worker: Caching app shell');
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activating...');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Service Worker: Deleting old cache', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// Fetch event
 self.addEventListener('fetch', (event) => {
-  if (!event.request.url.startsWith(self.location.origin)) {
-    return;
-  }
-
-  if (event.request.method !== 'GET') {
-    return;
-  }
-
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
         if (response) {
           return response;
         }
+        return fetch(event.request);
+      }
+    )
+  );
+});
 
-        return fetch(event.request)
-          .then((fetchResponse) => {
-            if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
-              return fetchResponse;
-            }
-
-            const responseToCache = fetchResponse.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return fetchResponse;
-          })
-      })
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
